cd0b3526bf0e0ecbcced2a4ba21cd9d57fe7ad85
gaoluo <luo.gao@pccw.com>
2019-05-24 16:19:49 +0800

    vue demo

---
+++ src/app.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="text"> {{text}}</div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            text: '我来了'
+        }
+    }
+}
+</script>
+<style>
+    #text{
+        color: red;
+    }
+</style>
